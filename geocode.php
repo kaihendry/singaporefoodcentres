2e0b2e8e6bcbeb3492e44267daa05142a1f75f52
Kai Hendry <hendry@webconverger.com>
2013-08-25 22:29:27 +0800

    Initial based on http://maps.natalian.org/

---
+++ geocode.php
@@ -0,0 +1,26 @@
+<?php
+$row = 1;
+if (($handle = fopen("hawkers.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        //echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+           // echo $data[$c] . "<br />\n";
+        }
+		//echo "WRITE: $data[0] " . $data[1] . "\n";
+		$geodec = "http://maps.googleapis.com/maps/api/geocode/json?address=" . urlencode($data[1]) .  "&sensor=false";
+		$test = json_decode(file_get_contents($geodec));
+		//print_r($test);
+		//echo $test->status;
+		echo $data[0] . ":";
+		if ($test->status == "OK") {
+		echo $test->results[0]->geometry->location->lat . ":";
+		echo $test->results[0]->geometry->location->lng;
+		}
+		echo "\n";
+		//return;
+    }
+    fclose($handle);
+}
+?>
---
+++ mymap.js
@@ -0,0 +1,41 @@
+function downloadUrl(url, callback) {
+	var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
+	request.onreadystatechange = function() {
+		// console.log(request.readyState);
+		if (request.readyState == 4) {
+			callback(request.responseText, request.status);
+		}
+	};
+	request.open('GET', url, true);
+	request.send(null);
+}
+
+function initialize() {
+	var myLatlng = new google.maps.LatLng(1.3010,103.8600);
+	var myOptions = {
+		zoom: 12,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+	downloadUrl("data.txt", function(data, responseCode) {
+
+		if (responseCode == 200) {
+			lines = data.split('\n');
+			for (var i = 0; i < lines.length - 1; i++) {
+
+				g = lines[i].split(':');
+				//console.log("X:" + g[2] + "Y:" + g[3]);
+				var p = new google.maps.LatLng(parseFloat(g[1]), parseFloat(g[2]));
+
+				var marker = new google.maps.Marker({
+					position: p,
+					map: map,
+					title: g[0]
+				});
+
+			}
+		} else alert("Request resulted in error with responseCode: " + responseCode);
+	});
+}
