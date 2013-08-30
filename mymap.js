7c8b1f325fbfbd997943bdffd9fe3b0db62f1c13
Kai Hendry <hendry@webconverger.com>
2013-08-30 23:43:16 +0800

    Get directions through the assumed installed Google maps application on IOS
    How to tell it's not installed?
    
    What about Android?

--- mymap.js
+++ mymap.js
@@ -1,10 +1,13 @@
+var iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true: false);
+
 function initialize() {
 	var myLatlng = new google.maps.LatLng(1.3010,103.8600);
 
 	var myOptions = {
 		zoom: 12,
 		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true,
 	}
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
@@ -15,18 +18,22 @@ function initialize() {
 			map.setCenter(pos);
 		});}
 
+	var infowindow = new google.maps.InfoWindow({ maxWidth: 260 });
+
 	function createMarker(h) {
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(h.lat, h.lng),
-			map: map, 
+			map: map,
 			title: h.place_name
 		});
 
-		var infowindow = new google.maps.InfoWindow({
-			content: "<h1>" + h.place_name + "</h1><p>" + h.place_desc + "</p>"
-		});
-
 		google.maps.event.addListener(marker, 'click', function() {
+			var s = "<h3>" + h.place_name + "</h3><p>" + h.place_desc + "</p>";
+			if (iOS) {
+				//s += '<a href="maps://?q=' + h.place_location + '">map</a></li>';
+				s += '<p><a href="comgooglemaps://?q=' + h.place_location + '">Directions</a></p>';
+			}
+			infowindow.setContent(s);
 			infowindow.open(map,marker);
 		});
 		return marker;
